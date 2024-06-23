a44825910d0ad685003deea0d0d19580f045d980
LukaStjc <luka2001stajic@gmail.com>
2024-06-23 13:38:42 +0200

    Added Player in Model and his CRUD => Controller, Service, Repository

---
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/model/Player.java
@@ -0,0 +1,69 @@
+package rs.ac.uns.acs.nais.GraphDatabaseService.model;
+
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Id;
+
+@Node
+public class Player {
+    @Id
+    private Long idOriginal;
+
+    private String name;
+
+    private String lastName;
+
+    private String position;
+
+    private int age;
+
+//    private Team team;
+
+
+    public Player(Long idOriginal, String name, String lastName, String position, int age) {
+        this.idOriginal = idOriginal;
+        this.name = name;
+        this.lastName = lastName;
+        this.position = position;
+        this.age = age;
+    }
+
+    public Long getIdOriginal() {
+        return idOriginal;
+    }
+
+    public void setIdOriginal(Long idOriginal) {
+        this.idOriginal = idOriginal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ GraphDatabaseService/src/main/java/rs/ac/uns/acs/nais/GraphDatabaseService/repository/PlayerRepository.java
@@ -0,0 +1,15 @@
+package rs.ac.uns.acs.nais.GraphDatabaseService.repository;
+
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.stereotype.Repository;
+import rs.ac.uns.acs.nais.GraphDatabaseService.model.Player;
+
+@Repository
+public interface PlayerRepository extends Neo4jRepository<Player, Long> {
+
+    @Query("match (p:Player {idOriginal: $id}) return p")
+    Player findByIdOriginal(Long id);
+
+
+}
